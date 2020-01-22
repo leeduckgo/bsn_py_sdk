9f1bad6e2cd8428cbfccc09d7f509f80f3ecd91c
leeduckgo <albertschr@163.com>
2020-01-22 22:35:06 +0800

    feat: tag v0.1.3

--- setup.py
+++ setup.py
@@ -1,16 +1,21 @@
 from setuptools import setup, find_packages
-
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 setup(
     # 以下为必需参数
     name='bsn-sdk',  # 模块名
-    version='0.1.2',  # 当前版本
+    version='0.1.3',  # 当前版本
     description='SDK for BSN(Blockchain Service Network)',  # 简短描述
     packages=['bsn_sdk','bsn_sdk.common'], # 单文件模块写法
     install_requires=['certifi','requests', 'cryptography', 'fabric-sdk-py'],
     # ckages=find_packages(exclude=['contrib', 'docs', 'tests']),  # 多文件模块写法
 
     # 以下均为可选参数
-    long_description="bsn-python-sdk is a Python library for communicating with [Blockchain Service Network](https://www.bsnbase.com/), It is used for building blockchain Apps with BSN.",# 长描述
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url='https://github.com/leeduckgo/bsn_py_sdk', # 主页链接
     author='李大狗Leeduckgo@SUIBE', # 作者名
     author_email='leeduckgo@gmail.com', # 作者邮箱

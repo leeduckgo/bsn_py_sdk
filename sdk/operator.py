c17ea4787676136ffa604683050b331a64dba811
leeduckgo <albertschr@163.com>
2020-01-22 16:03:22 +0800

    feat: optimize Operator

--- sdk/operator.py
+++ sdk/operator.py
@@ -59,6 +59,7 @@ class Operator:
     def __handle_with_key(self, userCode, appCode, chainCode, url, cert_path, funcName, baseKey):
         if len(baseKey.strip()) == 0:
             logger.error("baseKey can not be null")
+            return {"success": False, "payload": "incorrect basekey"}
         else:
             payload = userCode + appCode + chainCode + funcName + baseKey
             logger.info('baseKey: %s', baseKey)
@@ -72,13 +73,15 @@ class Operator:
                     "mac": mac}
             headers = {'content-type': 'application/json'}
             res = requests.post(url, headers=headers, json=data, verify= cert_path + '/bsn_https.pem')
-            return res.json()
+            return  self.__handle_res(res.json(), funcName)
 
     def __handle_with_key_and_value(self, userCode, appCode, chainCode, url, cert_path, funcName, baseKey, baseInfo):
         if len(baseKey.strip()) == 0:
             logger.error("baseKey can not be null")
+            return {"success": False, "payload": "incorrect basekey"}
         elif len(baseInfo.strip()) == 0:
             logger.error("baseInfo can not be null")
+            return {"success": False, "payload": "incorrect baseInfo"}
         else:
             logger.info('baseKey: %s', baseKey)
             logger.info('baseInfo: %s', baseInfo)
@@ -95,4 +98,25 @@ class Operator:
                     "mac": mac}
             headers = {'content-type': 'application/json'}
             res = requests.post(url, headers=headers, json=data, verify= cert_path + '/bsn_https.pem')
-            return res.json()
+            return  self.__handle_res(res.json(), funcName)
+
+    def __handle_res(self, payloadDicted, funcName):
+        try:
+            if payloadDicted['header']['code'] == 0:
+                txId = payloadDicted['body']['blockInfo']['txId']
+                context = self.__handle_payload(payloadDicted['body']['ccRes']['ccData'], funcName)
+                return {"success": True,"payload": {"txId": txId, "context": context}}
+            else:
+                error_msg = payloadDicted["header"]
+                logger.error('error when req from node: %s', error_msg)
+                return {"success": False, "payload": error_msg}
+        except:
+            logger.error('error when req from node: %s', payloadDicted)
+            return {"success": False, "payload": "unknow error"}
+    def __handle_payload(self, payload, funcName):
+        if funcName in ["save", "get", "delete", "update"]:
+            return payload
+        elif funcName == "getHistory":
+            return json.loads(payload)
+        
+

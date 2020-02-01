6a3b899be378d45531fe9b8aee1a1787617655ff
leeduckgo <albertschr@163.com>
2020-02-01 21:42:05 +0800

    feat: add blockchain Class and balabala

--- demo/app/views.py
+++ demo/app/views.py
@@ -6,9 +6,37 @@ from flask import (
 from app import app
 import json
 from functools import wraps
+from blockchain import Blockchain
+
+blockchain = Blockchain()
 
 #===init plugins====
 '''此处是插件'''
+"""
+    api follow the RESTFUL: http://www.ruanyifeng.com/blog/2014/05/restful_api.html
+"""
+
+# === api ===
+
+@app.route('/api/get_data', methods=['get'])
+def get_data():
+
+
+@app.route('/api/get_history', methods=['get'])
+def get_history():
+
+
+
+@app.route('/api/delete_data', methods=['get'])
+def delete_data():
+
+
+
+@app.route('/api/save_data', methods=['post'])
+def save_data():
+
+@app.route('/api/update_data', methods=['post'])
+def update_data():
 
 #===cross_domain====
 '''这个是允许跨域的代码'''
@@ -157,310 +185,6 @@ def do_transfer(key, to_addr, value):
     signed_tx = w3.eth.account.signTransaction(tx, key)
     tx_hash= w3.eth.sendRawTransaction(signed_tx.rawTransaction)
     return w3.eth.waitForTransactionReceipt(tx_hash)['transactionHash'].hex()
-
-
-"""
-    api follow the RESTFUL: http://www.ruanyifeng.com/blog/2014/05/restful_api.html
-"""
-# === get all items ===
-
-
-@app.route('/users', methods=['get'])
-def users():
-    items = User.query.all()
-    return msg_traslate_to_front(items)
-
-
-@app.route('/accs', methods=['get'])
-def accs():
-    items = Account.query.all()
-    return msg_traslate_to_front(items)
-
-
-@app.route('/orgs', methods=['get'])
-def orgs():
-    items = Org.query.all()
-    return msg_traslate_to_front(items)
-
-
-@app.route('/txs', methods=['get'])
-def txs():
-    items = Transaction.query.all()
-    return msg_traslate_to_front(items)
-
-# === get a item ===
-
-
-@app.route('/users/<name>', methods=['get'])
-def a_user(name):
-    item = User.query.filter_by(full_name = name).first()
-    if item:
-        resp = msg_traslate_to_front([item])
-    else:
-        resp = json.dumps({"result": "error"})
-    return resp
-
-
-@app.route('/accs/<address>', methods=['get'])
-def an_acc(address):
-    item = Account.query.filter_by(address = address).first()
-    if item:
-        resp = msg_traslate_to_front([item])
-    else:
-        resp = json.dumps({"result": "error"})
-    return resp
-
-
-@app.route('/orgs/<name>', methods=['get'])
-def an_org(name):
-    item = Org.query.filter_by(name = name).first()
-    if item:
-        resp = msg_traslate_to_front([item])
-    else:
-        resp = json.dumps({"result": "error"})
-    return resp
-
-
-@app.route('/txs/<id>', methods=['get'])
-def a_tx(tx_id):
-    item = Transaction.query.filter_by(tx_id = tx_id).first()
-    if item:
-        resp = msg_traslate_to_front([item])
-    else:
-        resp = json.dumps({"result": "error"})
-    return resp
-
-
-# === post(create) ===
-
-@app.route('/users', methods=['post'])
-def users_create():
-    jsoned = trans(request.get_data())
-    try:
-        item = User(full_name=jsoned["full_name"], student_id=jsoned["student_id"], org_id=jsoned["org_id"])
-        db.session.add(item)
-        db.session.commit()
-        return msg_traslate_to_front([item])
-    except:
-        return json.dumps({"result": "error"})
-
-
-# === update ===
-@app.route('/users/<name>', methods=['patch', 'post'])
-def users_update(name):
-    try:
-        jsoned = trans(request.get_data())
-        item = User.query.filter_by(full_name=name).first()
-        if jsoned["full_name"]:
-            item.full_name = jsoned["full_name"]
-        if jsoned["org_id"]:
-            item.org_id = jsoned["org_id"]
-        if jsoned["student_id"]:
-            item.student_id = jsoned["student_id"]
-        db.session.add(item)
-        db.session.commit()
-        return msg_traslate_to_front([item])
-    except:
-        return json.dumps({"result": "error"})
-
-
-# === delete ===
-@app.route('/users/<name>', methods=['delete'])
-def users_delete(name):
-    try:
-        jsoned = trans(request.get_data())
-        item = User.query.filter_by(full_name=name).first()
-        db.session.delete(item)
-        db.session.commit()
-        return msg_traslate_to_front([item])
-    except:
-        return json.dumps({"result": "error"})
-# def sth():
-#     #查询用户
-#     if request.form.get("user_name_queried"):
-#         user = User.query.filter_by(full_name=request.form.get("user_name_queried")).first()
-#         if user:
-#             resp = msg_traslate_to_front([user])
-#         else:
-#             resp = json.dumps({"result": "error"})
-#         return resp
-#     # 增加用户
-#     if request.form.get("full_name"):
-#         from models import db, User
-#         full_name = request.form.get("full_name")
-#         student_id = request.form.get("student_id")
-#         orgs_id = request.form.get("orgs_id")
-#         user = User(full_name=full_name, student_id=student_id, orgs_id=orgs_id)
-#         db.session.add(user)
-#         db.session.commit()
-#         return "保存成功"
-#     # 修改用户
-#     if request.form.get("upname"):
-#         from models import db, User
-#         user = User.query.filter_by(full_name=request.form.get("upname")).first()
-#         user.full_name = request.form.get("newfull_name")
-#         user.student_id = request.form.get("newstudent_id")
-#         user.orgs_id = request.form.get("neworgs_id")
-#         try:
-#             db.session.add(user)
-#             db.session.commit()
-#             return "修改成功"
-#         except Exception as e:
-#             return "修改失败"
-#     # 删除用户
-#     if request.form.get("delname"):
-#         from models import db, User
-#         user = User.query.filter_by(full_name=request.form.get("delname")).first()
-#         try:
-#             db.session.delete(user)
-#             db.session.commit()
-#             return "删除成功"
-#         except Exception as e:
-#             return "删除失败"
-#     return render_template('users.html')
-# #----------------------------以下基于accounts的操作-------------------------
-# @app.route('/accounts',methods=['get', 'post'])
-# def index2():
-#     # 查询account
-#     if request.form.get("seladdress"):
-#         from models import Account
-#         account = Account.query.filter_by(address=request.form.get("seladdress")).first()
-#         if account:
-#          return render_template('result.html', account=account)
-#         else:
-#          return "查询失败"
-#     # 增加account
-#     if request.form.get("address"):
-#         from models import db, Account
-#         address = request.form.get("address")
-#         balance = request.form.get("balance")
-#         account = Account(address=address, balance=balance)
-#         db.session.add(account)
-#         db.session.commit()
-#         return "保存成功"
-#     # 修改account
-#     if request.form.get("upaddress"):
-#         from models import db, Account
-#         account = Account.query.filter_by(address=request.form.get("upaddress")).first()
-#         account.address = request.form.get("newaddress")
-#         account.balance = request.form.get("newbalance")
-#         try:
-#             db.session.add(account)
-#             db.session.commit()
-#             return "修改成功"
-#         except Exception as e:
-#
-#             return "修改失败"
-#     # 删除account
-#     if request.form.get("deladdress"):
-#         from models import db, Account
-#         account = Account.query.filter_by(address=request.form.get("deladdress")).first()
-#         try:
-#             db.session.delete(account)
-#             db.session.commit()
-#             return "删除成功"
-#         except Exception as e:
-#             return "删除失败"
-#     return render_template('accounts.html')
-# #----------------------------以下基于org的操作-------------------------
-# @app.route('/organazations',methods=['get', 'post'])
-# def index3():
-#     # 查询org
-#     if request.form.get("selname"):
-#         from models import Account
-#         org = Org.query.filter_by(name=request.form.get("selname")).first()
-#         if org:
-#          return render_template('result.html', org=org)
-#         else:
-#          return "查询失败"
-#     # 增加org
-#     if request.form.get("name"):
-#         from models import db, Account
-#         name = request.form.get("name")
-#         account_id = request.form.get("account_id")
-#         org = Org(name=name, account_id=account_id)
-#         db.session.add(org)
-#         db.session.commit()
-#         return "保存成功"
-#     # 修改org
-#     if request.form.get("upname"):
-#         from models import db, Account
-#         org = Org.query.filter_by(name=request.form.get("upname")).first()
-#         org.name = request.form.get("newname")
-#         org.account_id = request.form.get("newaccount_id")
-#         try:
-#             db.session.add(org)
-#             db.session.commit()
-#             return "修改成功"
-#         except Exception as e:
-#
-#             return "修改失败"
-#     # 删除org
-#     if request.form.get("delname"):
-#         from models import db, Account
-#         org = Org.query.filter_by(name=request.form.get("delname")).first()
-#         try:
-#             db.session.delete(org)
-#             db.session.commit()
-#             return "删除成功"
-#         except Exception as e:
-#             return "删除失败"
-#     return render_template('organazations.html')
-# #----------------------------以下基于transaction的操作-------------------------
-# @app.route('/transactions',methods=['get', 'post'])
-# def index4():
-#     # 查询transcation
-#     if request.form.get("selid"):
-#         from models import Account
-#         tra = Transaction.query.filter_by(tx_id=request.form.get("selid")).first()
-#         # if transaction:
-#         return render_template('result.html', transaction=tra)
-#         # else:
-#         #     return "查询失败"
-#     # 增加transcation
-#     if request.form.get("addid"):
-#         from models import db, Account
-#         tx_id = request.form.get("addid")
-#         from_acc = request.form.get("from_acc")
-#         to_acc= request.form.get("to_acc")
-#         amount=request.form.get("amount")
-#         user_id=request.form.get("user_id")
-#         account_id=request.form.get("account_id")
-#         transaction=Transaction(tx_id=tx_id,from_acc=from_acc,to_acc=to_acc,amount=amount,user_id=user_id,account_id=account_id)
-#         db.session.add(transaction)
-#         db.session.commit()
-#         return "保存成功"
-#     # 修改transcation
-#     if request.form.get("upid"):
-#         from models import db, Account
-#         transcation = Transaction.query.filter_by(tx_id=request.form.get("upid")).first()
-#         transcation.tx_id = request.form.get("newtx_id")
-#         transcation.from_acc=request.form.get("newfrom_acc")
-#         transcation.to_acc=request.form.get("newto_acc")
-#         transcation.amount = request.form.get("newamount")
-#         transcation.user_id = request.form.get("newuser_id")
-#         transcation.account_id = request.form.get("newaccount_id")
-#         try:
-#             db.session.add(transcation)
-#             db.session.commit()
-#             return "修改成功"
-#         except Exception as e:
-#
-#             return "修改失败"
-#     # 删除transcation
-#     if request.form.get("delid"):
-#         from models import db, Account
-#         transcation = Transaction.query.filter_by(tx_id=request.form.get("delid")).first()
-#         try:
-#             db.session.delete(transcation)
-#             db.session.commit()
-#             return "删除成功"
-#         except Exception as e:
-#             return "删除失败"
-#     return render_template('transactions.html')
-
-#===   /pages   ===
-
 
 #=== black code ===
 
